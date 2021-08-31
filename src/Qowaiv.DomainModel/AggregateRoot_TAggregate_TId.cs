c34ad2c89b7ecb07c4c73b8118f9dbc27d90b314
Corniel Nobel <mail@corniel.nl>
2021-08-31 16:58:26 +0200

    AggregateRoot.FromStorage() should not clear the EventBuffer (#14)

--- src/Qowaiv.DomainModel/AggregateRoot_TAggregate_TId.cs
+++ src/Qowaiv.DomainModel/AggregateRoot_TAggregate_TId.cs
@@ -48,10 +48,10 @@ namespace Qowaiv.DomainModel
         }
 
         /// <summary>Loads the state of the aggregate root by replaying events.</summary>
-        internal void Replay(EventBuffer<TId> eventBuffer)
+        internal void Replay(EventBuffer<TId> buffer)
         {
-            Buffer = new EventBuffer<TId>(eventBuffer.AggregateId, eventBuffer.Version);
-            Replay(eventBuffer.AsEnumerable());
+            Buffer = buffer;
+            Replay(buffer.AsEnumerable());
         }
     }
 }
