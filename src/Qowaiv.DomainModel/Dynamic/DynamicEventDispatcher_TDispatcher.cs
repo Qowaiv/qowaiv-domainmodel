354b3b923d73de7c32c4493ae580bdaa041ee858
Corniel Nobel <mail@corniel.nl>
2022-04-09 13:22:26 +0200

    Use the DynamicEventDispatcher to build a super lightweight projector.
    Added documentation.
    
    
    Use a ReadOnlySet to be able to quickly determine if the dispatcher supports an event type.
    
    Descriptive method name.
    
    drop project.
    
    Update coverage.
    
    clean-up.

---
+++ src/Qowaiv.DomainModel/Dynamic/DynamicEventDispatcher_TDispatcher.cs
@@ -0,0 +1,114 @@
+﻿using System.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Qowaiv.DomainModel.Dynamic;
+
+/// <summary>A dynamic event dispatcher, is a extremely limited dynamic object
+/// that is capable of invoking instance methods with the signature When(@event).
+/// </summary>
+/// <typeparam name="TDispatcher">
+/// The type of the non dynamic event dispatcher.
+/// </typeparam>
+/// <remarks>
+/// The constraints on the method:
+/// * Name of the method is 'When'
+/// * Binding is on instance (both public and non-public)
+/// * One parameter with a type that is/could be an event.
+/// * Return type is ignored.
+///
+/// It caches the available methods per type.
+/// </remarks>
+public class DynamicEventDispatcher<TDispatcher> : DynamicEventDispatcher
+    where TDispatcher : class
+{
+    /// <summary>Initializes a new instance of the <see cref="DynamicEventDispatcher{TDispatcher}"/> class..</summary>
+    public DynamicEventDispatcher(TDispatcher dispatcher)
+        => this.dispatcher = Guard.NotNull(dispatcher, nameof(dispatcher));
+
+    private readonly TDispatcher dispatcher;
+
+    /// <summary>Tries to invoke a (void) When(@event) method.</summary>
+    /// <exception cref="EventTypeNotSupported">
+    /// If the invoke call was on (void) When(@event) but the type was not available.
+    /// </exception>
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        => binder?.Name == nameof(When) && args?.Length == 1 && !(args[0] is null)
+        ? When(args[0], out result)
+        : base.TryInvokeMember(binder, args, out result);
+
+    /// <summary>Gets the supported event types.</summary>
+    public sealed override ReadOnlySet<Type> SupportedEventTypes => Supported;
+
+    /// <summary>Invokes the When(@event) method.</summary>
+    private bool When(object @event, out object result)
+    {
+        var eventType = @event.GetType();
+        if (Lookup.TryGetValue(eventType, out var when))
+        {
+            result = null;
+            when(dispatcher, @event);
+            return true;
+        }
+        throw new EventTypeNotSupported(eventType, typeof(TDispatcher));
+    }
+
+    /// <summary>Initializes all When(@event) methods.</summary>
+    [Pure]
+    private static Dictionary<Type, Action<TDispatcher, object>> Init()
+    {
+        var lookup = new Dictionary<Type, Action<TDispatcher, object>>();
+
+        foreach (var method in WhenMethods())
+        {
+            var parameters = method.GetParameters();
+            if (parameters.Length == 1)
+            {
+                var parameterType = parameters[0].ParameterType;
+
+                // Leave out object itself, primitives and enumerations.
+                if (parameterType != typeof(object) &&
+                    !parameterType.IsPrimitive &&
+                    !parameterType.IsEnum)
+                {
+                    lookup[parameterType] = Compile(method, parameterType);
+                }
+            }
+        }
+        Supported = new ReadOnlySet<Type>(lookup.Keys);
+        return lookup;
+    }
+
+    /// <summary>Uses reflection to resolve `When` methods to use,
+    /// both non-public (mostly internal) and public.
+    /// </summary>
+    [Pure]
+    private static IEnumerable<MethodInfo> WhenMethods()
+        => typeof(TDispatcher)
+        .GetMethods(MethodSignature)
+        .Where(method => method.Name == nameof(When));
+
+    [Pure]
+    private static Action<TDispatcher, object> Compile(MethodInfo method, Type eventType)
+    {
+        var dispatcherParam = Expression.Parameter(typeof(TDispatcher), "dispatcher");
+        var @event = Expression.Parameter(typeof(object), "e");
+
+        var typedEvent = Expression.Convert(@event, eventType);
+        var body = Expression.Call(dispatcherParam, method, typedEvent);
+
+        var expression = Expression.Lambda<Action<TDispatcher, object>>(body, dispatcherParam, @event);
+
+        return expression.Compile();
+    }
+
+    private static readonly Dictionary<Type, Action<TDispatcher, object>> Lookup = Init();
+#pragma warning disable S2743 // Static fields should not be used in generic types
+    // Intended behavior, should only be shared between the same dispatcher types. 
+    private static ReadOnlySet<Type> Supported;
+#pragma warning restore S2743 // Static fields should not be used in generic types
+#pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
+    /// <remarks>We explicitly </remarks>
+    private const BindingFlags MethodSignature = BindingFlags.Public | BindingFlags.NonPublic| BindingFlags.Instance;
+#pragma warning restore S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
+}
--- test/Qowaiv.DomainModel.UnitTests/Dynamic/DynamicEventDispatcher_specs.cs
+++ test/Qowaiv.DomainModel.UnitTests/Dynamic/DynamicEventDispatcher_specs.cs
@@ -1,100 +1,142 @@
 ﻿using Microsoft.CSharp.RuntimeBinder;
-using NUnit.Framework;
+using Qowaiv.DomainModel;
 using Qowaiv.DomainModel.Dynamic;
-using System;
 
-namespace Dynamic_event_dispatcher_specs
+namespace Dynamic_event_dispatcher_specs;
+
+public class Support_when_methods
 {
-    public class Support_when_methods
+    [Test]
+    public void With_single_argument() => AssertEventTypes<WhenWithSingleArgument>();
+    [Test]
+    public void With_return_statement() => AssertEventTypes<WhenWithReturnStatement>();
+    [Test]
+    public void Non_public_methods()
     {
-        [Test]
-        public void With_single_argument() => AssertEventTypes<WhenWithSingleArgument>();
-        [Test]
-        public void With_return_statement() => AssertEventTypes<WhenWithReturnStatement>();
-        [Test]
-        public void Non_public_methods()
-        {
-            dynamic dispatcher = new DynamicEventDispatcher<WhenWithSingleArgument>(new WhenWithSingleArgument());
-            Assert.Catch<Detected>(() => dispatcher.When(new DummyEvent()));
-        }
-
-        internal class WhenWithSingleArgument
-        {
-            internal void When(DummyEvent @event) => throw new Detected();
-        }
-
-        internal class WhenWithReturnStatement
-        {
-            internal int When(DummyEvent @event) => throw new Detected();
-        }
-
-        private static void AssertEventTypes<T>() where T : class, new()
-        {
-            var dynamic = new DynamicEventDispatcher<T>(new T());
-            Assert.AreEqual(1, dynamic.SupportedEventTypes.Count);
-        }
-
-        internal class DummyEvent { }
-
-        public class Detected : Exception { }
+        dynamic dispatcher = new DynamicEventDispatcher<WhenWithSingleArgument>(new WhenWithSingleArgument());
+        Assert.Catch<Detected>(() => dispatcher.When(new DummyEvent()));
     }
 
-    public class Not_supported_when_methods
+    internal class WhenWithSingleArgument
     {
-        [Test]
-        public void Object_argument() => AssertNoEventTypes<WhenWithObjectParameter>();
-        
-        [Test]
-        public void Primitive_arguments() => AssertNoEventTypes<WhenWithPrimitiveParameters>();
-
-        [Test]
-        public void Enum_arguments() => AssertNoEventTypes<WhenWithEnumParameters>();
-
-        [Test]
-        public void No_arguments() => AssertNoEventTypes<WhenNoParameterEvent>();
-
-        [Test]
-        public void Multiple_arguments() => AssertNoEventTypes<WhenWithMultipleParameters>();
-
-        private static void AssertNoEventTypes<T>() where T: class, new()
-        {
-            var dynamic = new DynamicEventDispatcher<T>(new T());
-            Assert.IsEmpty(dynamic.SupportedEventTypes);
-        }
-
-        [Test]
-        public void Fallback_to_base()
-        {
-            dynamic dynamic = new DynamicEventDispatcher<WhenWithMultipleParameters>(new WhenWithMultipleParameters());
-            Assert.IsEmpty(dynamic.SupportedEventTypes);
-            Assert.Throws<RuntimeBinderException>(() => dynamic.When(new DummyEvent { }, 4));
-        }
-
-        internal class WhenWithMultipleParameters
-        {
-            internal void When(DummyEvent @event, int other) => throw new NotSupportedException($"{@event} + {other}.");
-        }
-
-        internal class WhenNoParameterEvent
-        {
-            internal void When() => throw new NotSupportedException();
-        }
-
-        internal class WhenWithObjectParameter
-        {
-            internal void When(object @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
-        }
-        internal class WhenWithPrimitiveParameters
-        {
-            internal void When(int @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
-            internal void When(long @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
-            internal void When(bool @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
-        }
-        internal class WhenWithEnumParameters
-        {
-            internal void When(Base64FormattingOptions @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
-            internal void When(EnvironmentVariableTarget @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
-        }
-        internal class DummyEvent { }
+        internal void When(DummyEvent @event) => throw new Detected();
     }
+
+    internal class WhenWithReturnStatement
+    {
+        internal int When(DummyEvent @event) => throw new Detected();
+    }
+
+    private static void AssertEventTypes<T>() where T : class, new()
+    {
+        var dynamic = new DynamicEventDispatcher<T>(new T());
+        Assert.AreEqual(1, dynamic.SupportedEventTypes.Count);
+    }
+
+    internal class DummyEvent { }
+
+    public class Detected : Exception { }
+}
+
+public class Not_supported_when_methods
+{
+    [Test]
+    public void Object_argument() => AssertNoEventTypes<WhenWithObjectParameter>();
+    
+    [Test]
+    public void Primitive_arguments() => AssertNoEventTypes<WhenWithPrimitiveParameters>();
+
+    [Test]
+    public void Enum_arguments() => AssertNoEventTypes<WhenWithEnumParameters>();
+
+    [Test]
+    public void No_arguments() => AssertNoEventTypes<WhenNoParameterEvent>();
+
+    [Test]
+    public void Type_without_matching_When_method()
+    {
+        dynamic dispatcher = new DynamicEventDispatcher<WhenWithDummyEvent>(new WhenWithDummyEvent());
+        Action when = () => dispatcher.When(new NotRegisteredEvent());
+        when.Should().Throw<EventTypeNotSupported>();
+    }
+
+    [Test]
+    public void Multiple_arguments() => AssertNoEventTypes<WhenWithMultipleParameters>();
+
+    private static void AssertNoEventTypes<T>() where T: class, new()
+    {
+        var dynamic = new DynamicEventDispatcher<T>(new T());
+        Assert.IsEmpty(dynamic.SupportedEventTypes);
+    }
+
+    [Test]
+    public void Fallback_to_base()
+    {
+        dynamic dynamic = new DynamicEventDispatcher<WhenWithMultipleParameters>(new WhenWithMultipleParameters());
+        Assert.IsEmpty(dynamic.SupportedEventTypes);
+        Assert.Throws<RuntimeBinderException>(() => dynamic.When(new DummyEvent { }, 4));
+    }
+
+
+    internal class WhenWithDummyEvent
+    {
+        internal void When(DummyEvent @event) => throw new Detected();
+    }
+
+    internal class WhenWithMultipleParameters
+    {
+        internal void When(DummyEvent @event, int other) => throw new NotSupportedException($"{@event} + {other}.");
+    }
+
+    internal class WhenNoParameterEvent
+    {
+        internal void When() => throw new NotSupportedException();
+    }
+
+    internal class WhenWithObjectParameter
+    {
+        internal void When(object @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
+    }
+    internal class WhenWithPrimitiveParameters
+    {
+        internal void When(int @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
+        internal void When(long @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
+        internal void When(bool @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
+    }
+    internal class WhenWithEnumParameters
+    {
+        internal void When(Base64FormattingOptions @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
+        internal void When(EnvironmentVariableTarget @event) => throw new NotSupportedException($"Not a valid event: {@event}.");
+    }
+    internal class DummyEvent { }
+    internal class NotRegisteredEvent { }
+
+    public class Detected : Exception { }
+}
+
+public class Can_be_created
+{
+    [Test]
+    public void with_generic_constructor()
+    {
+        dynamic dispatcher = new DynamicEventDispatcher<WhenWithDummyEvent>(new WhenWithDummyEvent());
+        Action when = () => dispatcher.When(new DummyEvent());
+        when.Should().Throw<Detected>();
+    }
+    [Test]
+    public void with_non_generic_factory()
+    {
+        dynamic dispatcher = DynamicEventDispatcher.New(new WhenWithDummyEvent());
+        Action when = () => dispatcher.When(new DummyEvent());
+        when.Should().Throw<Detected>();
+    }
+
+    internal class WhenWithDummyEvent
+    {
+        internal void When(DummyEvent @event) => throw new Detected();
+    }
+
+    internal class DummyEvent { }
+
+    public class Detected : Exception { }
 }
