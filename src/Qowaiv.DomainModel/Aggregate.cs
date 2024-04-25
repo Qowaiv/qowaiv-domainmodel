450434dcbbc9cafb6ae06643a8edad81139d6bdc
Corniel Nobel <corniel@gmail.com>
2024-04-25 10:39:56 +0200

    Expose supported event types (#41)

--- src/Qowaiv.DomainModel/Aggregate.cs
+++ src/Qowaiv.DomainModel/Aggregate.cs
@@ -1,4 +1,6 @@
-﻿namespace Qowaiv.DomainModel;
+﻿using System.Reflection;
+
+namespace Qowaiv.DomainModel;
 
 /// <summary>Factory method for creating <see cref="Aggregate{TAggregate, TId}"/> from stored events.</summary>
 public static class Aggregate
@@ -23,4 +25,22 @@ public static class Aggregate
         aggregate.Replay(buffer);
         return aggregate;
     }
+
+    /// <inheritdoc cref="EventDispatcher.SupportedEventTypes" />
+    [Pure]
+    public static ReadOnlySet<Type> SupportedEventTypes<TAggregate>()
+        where TAggregate : Aggregate<TAggregate>, new()
+    {
+        return Dispatcher(new TAggregate()).SupportedEventTypes;
+
+        EventDispatcher Dispatcher(TAggregate aggregate)
+            => (EventDispatcher)typeof(TAggregate)
+                .GetProperty(nameof(Dispatcher), NonPublic)!
+                .GetValue(aggregate)!;
+    }
+
+#pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
+    // We own the code.
+    private const BindingFlags NonPublic = BindingFlags.Instance | BindingFlags.NonPublic;
+#pragma warning restore S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
 }
---
+++ test/Qowaiv.DomainModel.UnitTests/Supported_EventTypes_specs.cs
@@ -0,0 +1,19 @@
+﻿using Qowaiv.DomainModel;
+
+namespace Supported_EventTypes_specs;
+
+public class Exposes
+{
+    [Test]
+    public void Supported_EventTypes_of_protected_dispatcher()
+    {
+        Aggregate.SupportedEventTypes<SimpleEventSourcedAggregate>()
+            .Should().BeEquivalentTo(
+            [
+                typeof(NameUpdated),
+                typeof(DateOfBirthUpdated),
+                typeof(SimpleInitEvent),
+                typeof(InvalidEvent),
+            ]);
+    }
+}
