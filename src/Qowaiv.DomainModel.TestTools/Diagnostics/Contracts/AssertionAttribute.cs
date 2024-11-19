db886af4ab6802bec0fdfd60907b978eed95e9fc
Corniel Nobel <corniel@gmail.com>
2024-11-19 20:47:33 +0100

    Update to .NET 9.0 (#47)

--- src/Qowaiv.DomainModel.TestTools/Diagnostics/Contracts/AssertionAttribute.cs
+++ src/Qowaiv.DomainModel.TestTools/Diagnostics/Contracts/AssertionAttribute.cs
@@ -2,5 +2,5 @@
 
 /// <summary>To mark a method explicitly as impure.</summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-[Conditional("CONTRACTS_FULL")]
-public sealed class AssertionAttribute : ImpureAttribute { }
+//[Conditional("CONTRACTS_FULL")]
+internal sealed class AssertionAttribute : ImpureAttribute { }
