98097c75603e8c0e651d3074637fee9c6719020f
Corniel Nobel <corniel@gmail.com>
2024-11-19 21:43:08 +0100

    Sort.

--- src/Qowaiv.DomainModel.TestTools/Diagnostics/Contracts/AssertionAttribute.cs
+++ src/Qowaiv.DomainModel.TestTools/Diagnostics/Contracts/AssertionAttribute.cs
@@ -2,5 +2,5 @@ namespace Qowaiv.DomainModel.TestTools.Diagnostics.Contracts;
 
 /// <summary>To mark a method explicitly as impure.</summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-//[Conditional("CONTRACTS_FULL")]
+[Conditional("CONTRACTS_FULL")]
 internal sealed class AssertionAttribute : ImpureAttribute { }
